4bf569b578e71ad6e13b4e70fdeca93ce8d7ffa5
alberto8812 <cvelascosaavedra@gmail.com>
2023-09-20 06:21:16 -0500

    im pplement Provider in native with typescrip

--- App.tsx
+++ App.tsx
@@ -5,15 +5,27 @@ import { NavigationContainer } from '@react-navigation/native';
 //import { MyDrawer } from './src/Navigator/MyDrawer';
 import { MenuLateral } from './src/Navigator/MenuLateral';
 import { Tabs } from './src/Navigator/Tabs';
+import { AuthProvider } from './src/context/AuthContext';
 
 
 
  const App = () => (
    <NavigationContainer>
+    <AppState>
      {/* <StackNavigator /> */}
      {/* <MyDrawer /> */}
      <MenuLateral/>
      {/* <Tabs/> */}
+     </AppState>
    </NavigationContainer>
  )
+
+
+ const AppState=({children}:any) =>{
+  return(
+    <AuthProvider>
+      {children}
+    </AuthProvider>
+  )
+ }
 export default App;
